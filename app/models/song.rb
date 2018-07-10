03b784e47c3b4ec99932d932dcf347e5a93a8608
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-10 18:56:59 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,8 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def artist_name=(name)
+    self.artist_name = Artist.find_by(name: name)
+  end
+end
